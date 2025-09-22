0743a97f2f034a59223eb9b0fbfb783ec28d9dd9
Htet Khant Linn <htetkhantlinn123@gmail.com>
2025-09-22 08:40:30 +0630

    Add new hand-on exercise

---
+++ notes/Hand-on Examples/W4_DFS.py
@@ -0,0 +1,24 @@
+# Depth-First Search (DFS)
+
+
+# Using adjacency list representation
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': [],
+    'F': []
+}
+
+visited = set()
+
+def dfs(node):
+    if node not in visited:
+        print(node, end=" ")
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs(neighbor)
+
+# Run DFS from A
+dfs('A')
