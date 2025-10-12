5b965c86fe8d0ba8c52980e4ad9e3197fbfdafcf
Htet Khant Linn <htetkhantlinn123@gmail.com>
2025-10-12 20:55:48 +0630

    Update files

---
+++ assignments/2025-10-6_Discussion_2_Loops.py
@@ -0,0 +1,27 @@
+# a)
+print("Question 'a' Output: ")
+for i in range(5): 
+    print(i * i)
+
+print("------------------------")
+
+# b)
+print("\nQuestion 'b' Output: ")
+for d in [3,1,4,1,5]: 
+    print(d, end=" ")
+
+print("\n------------------------")
+
+# c)
+print("\nQuestion 'c' Output: ")
+for i in range(4): 
+    print("Hello")
+
+print("------------------------")
+
+# d)
+print("\nQuestion 'd' Output: ")
+for i in range(5): 
+    print(i, 2**i)
+
+print("------------------------")
