0581eda705833ffba1e1f96558a1142e47805727
Htet Khant Linn <htetkhantlinn123@gmail.com>
2025-10-18 19:18:52 +0630

    Upload 3 Discussion Assignment Files

---
+++ assignments/2025-10-18_Discussion_3_mathematical_expressions.py
@@ -0,0 +1,18 @@
+import math as m
+
+# a) 
+print((3 + 4) * 5)
+
+# b)
+print(n * (n - 1) / 2)
+
+# c)
+print(4 * m.pi * r**2)
+
+# d) 
+print(m.sqrt((r * (m.cos(a))**2) + (r * (m.sin(b)**2))))
+
+# e)
+print((y2 - y1) / (x2 - x1))
+
+
---
+++ assignments/2025-10-18_Discussion_3_sequence_of_numbers.py
@@ -0,0 +1,15 @@
+# a
+print(list(range(5)))
+
+# b
+print(list(range(3, 10)))
+
+# c
+print(list(range(4, 13, 3)))
+
+# d
+print(list(range(15, 5, -2)))
+
+# e
+print(list(range(5, 3)))
+
