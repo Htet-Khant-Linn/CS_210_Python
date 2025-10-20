eaa727eb9c431f865fbdca1df4423f37829dc61e
Htet Khant Linn <htetkhantlinn123@gmail.com>
2025-10-20 21:54:00 +0630

    Edit discussion assignment

--- assignments/2025-10-18_Discussion_3_sequence_of_numbers.py
+++ assignments/2025-10-18_Discussion_3_sequence_of_numbers.py
@@ -1,15 +1,20 @@
 # a
+print("\n--- A ---")
 print(list(range(5)))
 
 # b
+print("\n--- B ---")
 print(list(range(3, 10)))
 
 # c
+print("\n--- C ---")
 print(list(range(4, 13, 3)))
 
 # d
+print("\n--- D ---")
 print(list(range(15, 5, -2)))
 
 # e
+print("\n--- E ---")
 print(list(range(5, 3)))
 
