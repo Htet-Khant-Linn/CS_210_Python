8dd1648422a1f557d532acdc470c763285841a3d
Htet Khant Linn <htetkhantlinn123@gmail.com>
2025-10-29 08:39:23 +0630

    Update unicode number to text v2

---
+++ practice/2025-10-29_unicode_number_to_text_v2.py
@@ -0,0 +1,38 @@
+'''
+This program converts a sequence of unicode number into the string of text that it represents.
+'''
+
+
+def main():
+
+    print("This program converts a sequence of unicode number into \
+the string of text that it represents.\n")
+    
+    # accept the user input
+    input_str = input("Please enter the Unicode code message: ")
+
+    # create a message string
+    chars = []
+
+    # for loop to loop through Unicode number splitted by space
+    for num_str in input_str.split():
+        try:
+            # convert the sub-string to a number
+            num_int = int(num_str)
+
+            # append character to message
+            chars.append(chr(num_int))
+
+        except ValueError:
+            print(f"Invalid Input: [{num_str}] is not a valid Unicode code.")
+            continue # this will continue the program while showing only the error character
+
+        except Exception as e:
+            print(f"An error occured: {e}")
+            continue
+
+    message = "".join(chars)
+    print(f"\n The decoded message is:", message)
+
+
+main()
