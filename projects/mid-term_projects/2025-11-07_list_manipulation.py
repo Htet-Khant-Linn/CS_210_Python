f22eca7c8fc70277400ebd0440404132c0c6ce17
Htet Khant Linn <htetkhantlinn123@gmail.com>
2025-11-07 20:26:37 +0630

    Add list_manipulation.py

---
+++ projects/mid-term_projects/2025-11-07_list_manipulation.py
@@ -0,0 +1,22 @@
+'''
+Create a list with the numbers [10, 20, 30, 40, 50] and: 
+• Add 60 to the list 
+• Remove 30 from the list 
+• Print the final list 
+'''
+
+print("This is the list manipulation exercise.")
+print("Created by Htet Khant Linn.")
+print("------------------------------\n")
+
+my_list = [10, 20, 30, 40, 50]
+print(f"Initial list: {my_list}")
+
+my_list.append(60)
+print("Appending 60 to the list...")
+
+my_list.remove(30)
+print("Removing 30 from the list...")
+
+print(f"Final output: {my_list}")
+
