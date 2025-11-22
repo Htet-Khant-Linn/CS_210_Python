6bc49a2a4ec679eb71b4341c26b12ac9add904c3
Htet Khant Linn <htetkhantlinn123@gmail.com>
2025-11-22 11:31:35 +0630

    Update leap_year_checker.py

--- assignments/2025-11-20_Assignment_6_leap_year_checker.py
+++ assignments/2025-11-20_Assignment_6_leap_year_checker.py
@@ -7,31 +7,38 @@ Hints:
     • 1600 and 2000 are divisible by both 100 and 400 → Leap years.
 '''
 
+# create the function to check the leap year
 def check_leap_year(year):
-    if year > 0:
+    # use if-else to check whether the input value is non-negative
+    if year >= 0:
+        # apply the leap-year calculation formula
+        # use if-else to check the leap-year
         if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
             print(f"{year} is a leap year.")
         else:
-            print(f"{year} is not a leap year")
+            print(f"{year} is not a leap year.")
+        # this will print error message if the input is negative value
     else:
-        print(f"Invalid Input: Your input is '{year}'. The year value can't be a negative.")
+        print(f"Invalid Input: Your input is '{year}'. The year value can't be negative.")
 
 def main():
 
     print()
-    print("This program give check whether the year is a leap year or not.")
+    print("This program checks whether the year is a leap year or not.")
     print("Developed by Htet Khant Linn.")
     print("------------------------------\n")
 
-
+    # use try-except to catch the valueError
     try:
+        # accept the user input
         user_input_year = input("Please enter a year: ")
+        # change the input data to int
         year = int(user_input_year)
-
+        # apply the function
         check_leap_year(year)
 
     except ValueError:
-        print("Input Error: Please enter the valid year.")
+        print("Input Error: Please enter a valid year.")
 
 
 if __name__ == "__main__":
