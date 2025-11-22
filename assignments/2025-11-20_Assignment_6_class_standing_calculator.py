1abd30610af1a9a9a52e13b624c73cfc75052624
Htet Khant Linn <htetkhantlinn123@gmail.com>
2025-11-22 11:25:02 +0630

    update class_standing_calculator.py

--- assignments/2025-11-20_Assignment_6_class_standing_calculator.py
+++ assignments/2025-11-20_Assignment_6_class_standing_calculator.py
@@ -10,8 +10,11 @@ Hints:
 
 '''
 
+# create a function to check class standing based on the student's credits
 def class_standing_calculator(credits):
-    if credits > 0:
+    # check for negative credits
+    if credits >= 0:
+        # create the decision structure using if-elif-else
         if credits >= 26:
             print("Senior")
         elif credits >= 16:
@@ -21,6 +24,7 @@ def class_standing_calculator(credits):
         else:
             print("Freshman")
     else:
+        # will print an error message if the input is negative credit value
         print("Input Error: Credit Score can't be negative value.")
 
 def main():
@@ -30,14 +34,18 @@ def main():
     print("Developed by Htet Khant Linn.")
     print("------------------------------\n")
 
-
+    # use try-except to catch invalid inputs
     try:
+        # accept the user input
         user_input_credit = input("Please enter the number of credit earned: ")
+        # change the data type for float
         credits = float(user_input_credit)
 
+        # print out the result
         print("Student's year is: ")
-        class_standing_calculator(credits)
+        class_standing_calculator(credits)  # call the function
 
+    # use except to catch valueError
     except ValueError:
         print("Value Error: Please enter the valid credit score.")
 
